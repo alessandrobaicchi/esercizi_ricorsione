e8252c7fdcea43cdc086f9e998791170a4e2f602
Alessandro Baicchi <sasha.washi@gmail.com>
2026-04-15 19:54:51 +0200

    Lezione 14, Ricorsione parte1 commentata

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,30 +3,33 @@ from time import time
 
 class Fibonacci:
     def __init__(self):
-        self.cache = {0: 0, 1: 1}
+        self.cache = {0: 0, 1: 1}   # E' un dizionario che contiene (memorizza) le soluzioni già calcolate
 
     def calcola_elemento_cache(self, n):
-        # se ho già la soluzione per questo n
-        # la prendo dalla cache
+        # Se ho già la soluzione per questo n, allora la prendo dalla cache
         if self.cache.get(n) is not None:
+            # Se il valore di .get() non è None, significa che la soluzione già c'è,
+            # e quindi la restituisco.
             return self.cache[n]
-        # altrimenti devo andare avanti con la
-        # ricorsione
+        # Altrimenti devo calcolarla con la ricorsione
         else:
             self.cache[n] = (self.calcola_elemento_cache(n-1)
              + self.calcola_elemento_cache(n-2))
             return self.cache[n]
 
+
     def calcola_elemento(self,n):
-        # caso terminale
+        # condizione terminale
         if n ==0 :
             return 0
         elif n==1 :
             return 1
-        # caso ricorsivo
+        # condizione non terminale (ricorsivo)
         else:
             return (self.calcola_elemento(n-1) +
                     self.calcola_elemento(n-2))
+
+
     @lru_cache(maxsize=None)
     def calcola_elemento_lru(self,n):
         # caso terminale
--- nested_list.py
+++ nested_list.py
@@ -1,22 +1,26 @@
+# In realtà, in questo esercizio il caso terminale non serve.
+# Però, l'abbiamo messo per impratichirci ad usare la struttura classica della ricorsione: if/else.
+
 def count_leaf_nodes(input_list):
-    #terminale
+    # condizione terminale
     if len(input_list) == 0:
         return 0
-    # non terminale
+    # condizione non terminale
     else:
         counter = 0
         for element in input_list:
-            #check if element is a list
-            # if it is a list, we count its elements
-            # with a recursion
+            # Check if element is a list
             if type(element) == list:
                 counter += count_leaf_nodes(element)
-            # else, we add +1
+                # If it is a list, we count its elements with a recursion
+                # Cioè, aggiungo quello che count_leaf_nodes() troverà nella sottolista. E' ricorsione!
             else:
                 counter += 1
+                # else, we add +1
+                # Cioè, se non è una lista è un elemento da contare subito
         return counter
 
 
 if __name__ == '__main__':
     names = ['Adam', ['Bob', ['Chet', 'Cat'], 'Barb', 'Bert'], 'Alex', ['Bea', 'Bill'], 'Ann']
-    print(count_leaf_nodes(names)) #we expect 10
+    print(count_leaf_nodes(names)) # We expect 10
